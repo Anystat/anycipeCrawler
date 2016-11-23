7562613de7b116b92b055807cee6584ce825e591
Lofv <dredintellekt@bk.ru>
2016-11-23 15:34:02 +1000

    Сборка через Maven

---
+++ src/main/java/Crawler.java
@@ -0,0 +1,69 @@
+import java.util.*;
+
+
+public class Crawler {
+    private static final int MAX_PAGES_TO_SEARCH = 100;
+    String currentUrl;
+    CrawlerLeg leg;
+    private Set<String> pagesVisited = new HashSet<String>();
+    private List<String> pagesToVisit = new LinkedList<String>();
+
+    public void search(ArrayList url) {
+
+        leg = new CrawlerLeg();
+
+
+        for (int i = 0; i < url.size(); i++) {
+            pagesToVisit.add(url.get(i).toString());
+            for (int j = 0; j < pagesToVisit.size(); j++) {
+
+
+                if (pagesToVisit.isEmpty()) {
+                    currentUrl = url.get(i).toString();
+
+                } else {
+
+                    currentUrl = nextUrl();
+                }
+
+                leg.crawl(currentUrl); // Lots of stuff happening here. Look at the crawl method in CrawlerLeg
+                pagesToVisit.addAll(leg.getLinks());
+
+                System.out.println("\n**Done** Visited " + pagesVisited.size() + " web page(s)");
+
+                //  pars.parsing(CrawlerLeg.htmlDocument);
+
+               /* for (int j = 0; j < searchWord.size(); j++) {
+
+                    boolean success = leg.searchForWord(searchWord.get(j).toString());
+                    if (success) {
+                        System.out.println(String.format("**Success** Word %s found at %s", searchWord.get(j).toString(), currentUrl));
+                        // break;
+                    }
+                    this.pagesToVisit.addAll(leg.getLinks());
+                }
+                System.out.println("\n**Done** Visited " + this.pagesVisited.size() + " web page(s)");
+*/
+
+
+            }
+        }
+    }
+
+    /**
+     * Returns the next URL to visit (in the order that they were found). We also do a check to make
+     * sure this method doesn't return a URL that has already been visited.
+     *
+     * @return
+     */
+    private String nextUrl() {
+        String nextUrl;
+        do {
+            nextUrl = pagesToVisit.remove(0);
+        } while (pagesVisited.contains(nextUrl));
+        pagesVisited.add(nextUrl);
+        return nextUrl;
+    }
+
+
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,32 @@
+/**
+ * Created by Dred on 07.11.2016.
+ */
+public class Main {
+
+
+    public static void main(String[] args) {
+        String listOfSites = "anycipeCrawlerMaven\\src\\main\\resources\\ListOfSites.txt";
+        String listOfIngredients = "anycipeCrawlerMaven\\src\\main\\resources\\ListOfIngredients.txt";
+
+
+
+/*      https://coderwall.com/p/nvu6jq/how-to-create-a-web-crawler-in-java  чтение сайтов из дока
+        http://www.netinstructions.com/how-to-make-a-simple-web-crawler-in-java/
+        http://www.programmersforum.ru/showthread.php?t=58898 как примерно реализовать перевод падежа
+        http://www.javaprobooks.ru/java-%D0%BF%D1%80%D0%BE%D0%B3%D1%80%D0%B0%D0%BC%D0%BC%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D0%B5/%D0%BF%D1%80%D0%BE%D0%B3%D1%80%D0%B0%D0%BC%D0%BC%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D0%B5-%D0%BD%D0%B0-java-%D0%B8%D1%82%D0%B5%D1%80%D0%B0%D1%82%D0%BE%D1%80%D1%8B-%D0%B2-%D0%BA%D0%BE%D0%BB.html
+        http://www.netinstructions.com/how-to-make-a-simple-web-crawler-in-java/
+*/
+
+      //  MongoConnector mongoConnector = new MongoConnector();
+      //  mongoConnector.connectionOpen();
+
+        Crawler spider = new Crawler();
+        spider.search(Filereader.read(listOfSites));
+       // mongoConnector.mongoConnect("anycipe_crawler"); // для удаления содержимого коллекции
+      //  mongoConnector.connectionClose();
+
+
+    }
+
+
+}
