737851edeaceff4714c95554b2a9f98bc2deafc0
Lofv <dredintellekt@bk.ru>
2016-11-25 17:31:43 +1000

    Доделал рекурсия парсинга сайта. Можно удалять 2 класса

--- src/main/java/RecursionTestCrawler.java
+++ src/main/java/RecursionTestCrawler.java
@@ -10,35 +10,36 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
- * Created by 23 on 25.11.2016.
+ * Created by Lofv on 25.11.2016.
  */
 public class RecursionTestCrawler {
     private final String USER_AGENT = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/13.0.782.112 Safari/535.1";
     private String regex;
-    private int count;
     private ParsingSay7Info pars;
     private Pattern pattern;
     private Matcher matcher;
-
+    private String mainUrl;
     private Set<String> pagesVisited = new HashSet<String>();
     private List<String> pagesToVisit = new LinkedList<String>();
 
-    RecursionTestCrawler() {
-        // default constructor
+    RecursionTestCrawler() {    // default constructor
     }
 
     RecursionTestCrawler(ArrayList<String> urls) {
+
         for (String url : urls) {
-            testRecursion(url);
+            mainUrl = url;
+
+            testRecursion(mainUrl);
         }
     }
 
     public void testRecursion(String url) {
-        System.out.println("Start crawler with url = " + url);
+
         pars = new ParsingSay7Info();
 
 
-        regex = url.toString() + "(recipe/)|(linkz_start/).+$";
+        regex = "^" + mainUrl.toString() + "(cook/|recipe/|linkz_start).+$";
         pattern = Pattern.compile(regex);
 
         try {
@@ -47,41 +48,48 @@ public class RecursionTestCrawler {
 
             if ((connection.response().statusCode() == 200) && (connection.response().contentType().contains("text/html"))) {
                 if (url.contains("recipe/")) {
-
-                    System.out.println("\n**Visiting** Received web page at " + url);
                     pars.parsing(document);
-                } else {
-                    System.out.println("url didn`t consist /recipe/");
 
+                } else {
                     Elements linksOnPage = document.select("a[href]");
-                    count = 0;
 
                     for (Element link : linksOnPage) {
-
                         matcher = pattern.matcher(link.absUrl("href"));
 
                         while (matcher.find()) {
-
                             pagesToVisit.add(matcher.group());
-                            count++;
+
                         }
                     }
-                    System.out.println("Found (" + linksOnPage.size() + ") links" + " and were added (" + count + ") to the List");
                 }
-                testRecursion(nextUrl());
             } else {
                 System.out.println("Something wrong with site`s content or connection response");
             }
+
+            String nextUrl = nextUrl();//перебираем все ссылки, которые добавили ранее в список
+            if (nextUrl != null) {
+                testRecursion(nextUrl);
+            } else {
+                System.out.println("Конец кина, ссылки кончились");
+            }
+
+
         } catch (IOException ioe) {
             System.out.println("We were not successful in our HTTP request");
         }
+
     }
 
 
     private String nextUrl() {
         String nextUrl;
         do {
-            nextUrl = pagesToVisit.remove(0);
+            System.out.println("Pages left: " + pagesToVisit.size());
+            if (pagesToVisit.isEmpty()) {
+                return nextUrl = null;
+            } else {
+                nextUrl = pagesToVisit.remove(0);
+            }
         } while (pagesVisited.contains(nextUrl));
         pagesVisited.add(nextUrl);
         return nextUrl;
